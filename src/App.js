ee22cb1666e9f9341b27aa743236146f32307d98
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-06-25 19:21:41 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './assets/img/logo.png';
+import IconLinkToBottom from './assets/icons/LinkToBottom.svg';
+import VectorRight from './assets/icons/VectorRight.svg';
 import './App.css';
 
 function App() {
@@ -29,7 +31,10 @@ function App() {
               <li>
                 <a href="tel:+38 099 765 4321">+38 099 765 4321</a>
               </li>
-              <li>UA</li>
+              <li>
+                <p>Ua</p>
+                <img className="UaIcon" src={IconLinkToBottom} alt="" s />
+              </li>
             </ul>
           </header>
           <div className="Hero">
@@ -46,21 +51,21 @@ function App() {
             </div>
 
             <div className="BottomTextConteiner">
-              <div className="Box">
+              <div className="BoxOne">
                 <h3>10+ років досвіду</h3>
                 <p>
                   За стільки часу ми накопичили великі знання навички, що дає
                   нам змогу ефективно розв'язувати складні завдання
                 </p>
               </div>
-              <div className="Box">
+              <div className="BoxTwo">
                 <h3>5+ спеціалістів</h3>
                 <p>
                   Наші люди володіють глибокими знаннями в галузі інженерії, і
                   застосовують свої навички для розробки оптимізації процесів.
                 </p>
               </div>
-              <div className="Box">
+              <div className="BoxThree">
                 <h3>100+ проектів</h3>
                 <p>
                   Саме стільки роботи було зроблено для наших клієнтів. Відгуки
@@ -70,6 +75,40 @@ function App() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="Conteiner">
+        <section className="StagesSale">
+          <h2>Етапи продажу деталі</h2>
+          <div className="ConteinerStagesSale">
+            <div className="LeftContainer">
+              <div className="Box">
+                <h3>Оцінювання та подання заявки</h3>
+                <p>
+                  Першим кроком буде оцінка вашого автомобільного каталізатора.
+                  Ви заповнюєте заявку, надаючи інформацію про марку, модель і
+                  стан. Також ми можемо просити фотографії для більш точної
+                  оцінки
+                </p>
+                <button>
+                  Заявка <img src={VectorRight} />
+                </button>
+              </div>
+              <div className="Box">
+                <h3>Відправлення каталізатора та перевірка</h3>
+                <p>
+                  Після згоди із ціною, Вам надають інструкції про відправлення
+                  каталізатора. Зазвичай необхідно відправити поштою. Коли
+                  отримуєте каталізатор - ми здійснюємо перевірку, щоб
+                  переконатися до наданої інформації
+                </p>
+                <button>
+                  Заявка <img src={VectorRight} />
+                </button>
+              </div>
+            </div>
+            <div className="CenterConteiner"></div>
+          </div>
+        </section>
       </div>
     </>
   );
